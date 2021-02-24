36bb229c900cc78b6a21a228121299ce5a46424b
Pravin Xeona <pravin.xeona21@gmail.com>
2021-02-24 18:02:22 +1100

    'Implemented box shadow on hover for the images in 'my work' section

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const d_arrow = document.getElementById("downward-arrow");
+
 const my_work = document.getElementById("my-work");
+const skills = document.getElementById("skills");
+const contacts = document.getElementById("footer");
 
 d_arrow.addEventListener("click", function () {
   my_work.scrollIntoView({ behavior: "smooth" });
