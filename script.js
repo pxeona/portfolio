00e59ad92eb22f79c890165317c10f20f5081c0e
Pravin Xeona <pravin.xeona21@gmail.com>
2021-02-25 02:00:05 +1100

    Made the nav bar sticky till the end of the webpage

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const d_arrow = document.getElementById("downward-arrow");
 const nav_bar_items = document.querySelector(".nav-items");
 const nav_bar = document.getElementById("nav-bar");
+const header = document.getElementById("navigation");
+const homePage = document.getElementById("intro");
+
+const headerHeight = header.getBoundingClientRect().height;
 
 const handleHover = function (e) {
   if (e.target.classList.contains("nav-link")) {
@@ -19,6 +23,21 @@ const smoothScrolling = function (eventGen) {
   const location = document.querySelector(eventGen.getAttribute("href"));
   location.scrollIntoView({ behavior: "smooth" });
 };
+
+const addStickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav_bar.classList.add("fixedNav");
+  else nav_bar.classList.remove("fixedNav");
+};
+
+const obsParams = {
+  root: null,
+  threshold: 0,
+};
+
+const headerObs = new IntersectionObserver(addStickyNav, obsParams);
+headerObs.observe(header);
 
 d_arrow.addEventListener("click", (e) => {
   e.preventDefault();
