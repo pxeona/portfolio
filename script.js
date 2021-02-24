f663135897d732340440b262950eb74f8801b178
Pravin Xeona <pravin.xeona21@gmail.com>
2021-02-24 23:09:19 +1100

    Fade out animation for the nav bar items

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const d_arrow = document.getElementById("downward-arrow");
 const nav_bar_items = document.querySelector(".nav-items");
+const nav_bar = document.getElementById("nav-bar");
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const parent = e.target.closest("#nav-bar");
+    const siblings = parent.querySelectorAll(".nav-link");
+
+    siblings.forEach((el) => {
+      if (el !== e.target) {
+        el.style.opacity = this;
+      }
+    });
+  }
+};
+
+const smoothScrolling = function (eventGen) {
+  const location = document.querySelector(eventGen.getAttribute("href"));
+  location.scrollIntoView({ behavior: "smooth" });
+};
 
 d_arrow.addEventListener("click", (e) => {
   e.preventDefault();
@@ -14,7 +33,5 @@ nav_bar_items.addEventListener("click", (e) => {
   }
 });
 
-const smoothScrolling = function (eventGen) {
-  const location = document.querySelector(eventGen.getAttribute("href"));
-  location.scrollIntoView({ behavior: "smooth" });
-};
+nav_bar.addEventListener("mouseover", handleHover.bind(0.5));
+nav_bar.addEventListener("mouseout", handleHover.bind(1));
