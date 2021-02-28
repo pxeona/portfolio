8f9268fe4ed346b3ad93963a27aac9a14c2cf3c9
Pravin Xeona <pravin.xeona21@gmail.com>
2021-03-01 10:12:55 +1100

    Implemented effect that reveals sections upon scrolling

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+//DOM elements
+
 const d_arrow = document.getElementById("downward-arrow");
 const nav_bar_items = document.querySelector(".nav-items");
 const nav_bar = document.getElementById("nav-bar");
 const header = document.getElementById("navigation");
 const homePage = document.getElementById("intro");
+const sections = document.querySelectorAll(".section");
 
 const headerHeight = header.getBoundingClientRect().height;
+
+//Methods
 
 const handleHover = function (e) {
   if (e.target.classList.contains("nav-link")) {
@@ -26,19 +31,43 @@ const smoothScrolling = function (eventGen) {
 
 const addStickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) nav_bar.classList.add("fixedNav");
   else nav_bar.classList.remove("fixedNav");
 };
 
-const obsParams = {
+const loadOnScroll = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+//Observer parameters
+
+const obsParamsHeader = {
   root: null,
   threshold: 0,
   //rootMargin: `-${headerHeight}px`,
 };
 
-const headerObs = new IntersectionObserver(addStickyNav, obsParams);
+const obsParamsSection = {
+  root: null,
+  threshold: 0.1,
+};
+
+//Observers
+
+const headerObs = new IntersectionObserver(addStickyNav, obsParamsHeader);
 headerObs.observe(header);
+
+const sectionObs = new IntersectionObserver(loadOnScroll, obsParamsSection);
+
+sections.forEach(function (section) {
+  sectionObs.observe(section);
+  section.classList.add("section--hidden");
+});
+
+//Event listeners
 
 d_arrow.addEventListener("click", (e) => {
   e.preventDefault();
